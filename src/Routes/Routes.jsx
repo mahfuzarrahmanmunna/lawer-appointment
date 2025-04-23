5c9af8be19f980a8fd8cc8131e0147f20620dd56
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-24 05:02:19 +0600

    added loadingspinner sectoin

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,7 @@ import Home from "../Pages/Home/Home";
 import Blogs from "../Pages/Blogs/Blogs";
 import MyBookings from "../Pages/MyBookings/MyBookings";
 import Contact from "../Pages/ContactUs/Contact";
+import LoadingSpinner from "../Components/LoadingSpinner/LoadingSpinner";
 
 export const router = createBrowserRouter([
     {
@@ -12,7 +13,9 @@ export const router = createBrowserRouter([
         children: [
             {
                 path: '/',
-                Component: Home
+                Component: Home,
+                hydrateFallbackElement: <LoadingSpinner />,
+                loader: () => fetch('/lower.jsx')
             },
             {
                 path: 'blogs',
