20843f0377432f20fc2386f9486db0fd7765a938
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-04-24 03:08:15 +0600

    added footer and banner section

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div className='relative max-w-xl mx-auto'>
+            <div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
 import NavBar from '../Components/Navbar/NavBar';
+import Footer from '../Components/Footer/Footer';
 
 const Root = () => {
     return (
         <div className='font-mulish'>
             <NavBar />
             <Outlet />
+            <Footer />
         </div>
     );
 };
